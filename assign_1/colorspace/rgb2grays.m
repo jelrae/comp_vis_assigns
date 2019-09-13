eea148d811aafe84189dff14b09aeb0d0596a703
Ruihan Sun <sunx0495@umn.edu>
2019-09-13 22:15:17 +0200

    add colorspace, dont't merge with master

--- assign_1/colorspace/rgb2grays.m
+++ assign_1/colorspace/rgb2grays.m
@@ -1,14 +1,18 @@
 function [output_image] = rgb2grays(input_image)
 % converts an RGB into grayscale by using 4 different methods
-
+    [R, G, B] = getColorChannels(input_image);
 % ligtness method
+    lightness = (max(max(R,G),B) + min(min(R,G),B))./2; 
 
 % average method
- 
+    average = (R + G + B)./3;
+
 % luminosity method
+    luminosity = 0.21 * R + 0.72 * G + 0.07 * B;
 
 % built-in MATLAB function 
+    built_in = rgb2gray(input_image);
 
- 
+    output_image = cat(3,lightness, average, luminosity, built_in); 
 end
 
--- assign_1/colorspace/rgb2normedrgb.m
+++ assign_1/colorspace/rgb2normedrgb.m
@@ -1,5 +1,12 @@
 function [output_image] = rgb2normedrgb(input_image)
 % converts an RGB image into normalized rgb
-
+    [R, G, B] = getColorChannels(input_image);
+    output_image = input_image;
+    nR = R./(R + G + B);
+    nG = G./(R + G + B);
+    nB = B./(R + G + B);
+    output_image(:,:,1) = nR;
+    output_image(:,:,2) = nG;
+    output_image(:,:,3) = nB;
 end
 
