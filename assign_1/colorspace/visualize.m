4dd9aa4433d4bb658fbf480af6d9988ce6000aa7
Ruihan Sun <sunx0495@umn.edu>
2019-09-14 12:38:56 +0200

    Update colorspace final

--- assign_1/colorspace/visualize.m
+++ assign_1/colorspace/visualize.m
@@ -1,14 +1,16 @@
 function visualize(input_image)
-    % if it is not gray scale transformed img
+    % if it is not gray scale show 4 channels
     % if it is gray scale, show 4 different gray imgs
     s = size(input_image);
     % grayscale
     if s(3) == 4
         imglist = {input_image(:,:,1), input_image(:,:,2), 
-                    input_image(:,:,3), input_image(:,:,4)}
+                    input_image(:,:,3), input_image(:,:,4)};
         montage(imglist);
     else
-        imshow(input_image);
+        imglist = {input_image, input_image(:,:,1),
+                    input_image(:,:,2), input_image(:,:,3)};
+        montage(imglist);
     end
 end
 
