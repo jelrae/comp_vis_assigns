457920865580989da1afefee311419e5e8bc2947
benjamin.wang <wlongxiang1119@gmail.com>
2019-09-17 21:12:12 +0200

    cleanup

--- assign_1/color_constancy/v1_ben/AWB.m
+++ assign_1/color_constancy/v1_ben/AWB.m
@@ -19,23 +19,5 @@ new_I = cat(3, R_new, G_new, B_new);
 figure, subplot(121), imshow(I), title("original image");
 subplot(122), imshow(new_I), title("white balanced image");
 
-% % % % % 
-% this approach will fail when the dominant illumination is not neutral
-% grey. For example, for blue sky.
-% wikipedia: 
-% it does not account for all variations of illumination intensity 
-% and it is not dynamic, also due to the grey world assumption; 
-% when new objects appear in the scene it fails.
-
-% % % % % 
-% The perfect reflector algorithm
-% is based on the assumption that the brightest pixel in an image
-% corresponds to the white point. Specular or glossy surfaces reflect the actual color
-% of the light source because their reflectance functions are constant 
-% over a wide range of wavelengths.
-% The perfect reflector algorithm locates the brightest pixel
-% and assigns it as the reference white point.
-
-
 
 
