63b4b9619a324a43112d32d762b51310dfa848f3
benjamin.wang <wlongxiang1119@gmail.com>
2019-09-21 16:08:14 +0200

    Cleanup

--- assign_2/Part-1: Image Enhancement/test_grad.m
+++
@@ -1,5 +0,0 @@
-image = imread('./images/image2.jpg');
-%imshow(image);
-[Gx, Gy, im_magnitude,im_direction] = compute_gradient(image);
-imglist = {Gx, Gy, im_magnitude, im_direction};
-montage(imglist);
