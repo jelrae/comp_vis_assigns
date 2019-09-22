9141a6dc7efb0be2c44f170ca1ca06d21dacf75c
asnijders <44669359+asnijders@users.noreply.github.com>
2019-09-22 17:02:16 +0200

    Update denoise.m

--- assign_2/Part-1: Image Enhancement/denoise.m
+++ assign_2/Part-1: Image Enhancement/denoise.m
@@ -1,11 +1,17 @@
-function [ imOut ] = denoise( image, kernel_type, varargin)
+function [ imOut ] = denoise(image, kernel_type, varargin)
 
 switch kernel_type
     case 'box'
-        fprintf('Not implemented\n')
+        imOut = imboxfilt(image, varargin{1});
+%       varargin{1} denotes window size
     case 'median'
-        fprintf('Not implemented\n')
+        imOut = medfilt2(image,[varargin{1} varargin{1}]);
+%       second argument constructs [m n] matrix from varargin{1} input
+
     case 'gaussian'
-        fprintf('Not implemented\n')
+%       varargin{1} denotes sigma, varargin{2} denotes window size,
+%       Gauss2d => function G = gauss2D( sigma , kernel_size )
+        imOut = imfilter(image, gauss2D (varargin{1}, varargin{2}) );
+        
 end
 end
