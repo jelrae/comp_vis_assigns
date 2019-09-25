cfc051b0fc3f7dc51a4c6f55c57b85e0f3e5b0de
asnijders <44669359+asnijders@users.noreply.github.com>
2019-09-25 21:53:33 +0200

    Update q7_5_gaussian_PSNR.m

--- assign_2/Part-1: Image Enhancement/q7_5_gaussian_PSNR.m
+++ assign_2/Part-1: Image Enhancement/q7_5_gaussian_PSNR.m
@@ -1,29 +1,109 @@
-% What is the e?ect of the standard deviation on the PSNR? 
-% Report the results in a table and discuss.
-
+% Choose an appropriate window size and standard deviation and justify your choice. 
+% Show the denoised images in your report.
 
 gaussian_img = imread("image1_gaussian.jpg");
+original_img = imread("image1.jpg");
 
-%% PSNR with varying values of sigma, window = 3
+% %% gaussian filtering with varying values of sigma, window = 3
+% 
+gauss2 = denoise(gaussian_img, 'gaussian', 0.2, 3);
+myPSNR(original_img, gauss2)
 
-gauss1 = denoise(gaussian_img, 'gaussian', 1, 3);
-fprintf('PSNR for sigma = 1, window = 3')
-psnr_gauss1 = myPSNR(gaussian_img, gauss1);
+gauss4 = denoise(gaussian_img, 'gaussian', 0.4, 3);
+myPSNR(original_img, gauss4)
 
-gauss2 = denoise(gaussian_img, 'gaussian', 3, 3);
-fprintf('PSNR for sigma = 3, window = 3')
-psnr_gauss2 = myPSNR(gaussian_img, gauss2);
+gauss6 = denoise(gaussian_img, 'gaussian', 0.6, 3);
+myPSNR(original_img, gauss6)
 
-gauss3 = denoise(gaussian_img, 'gaussian', 5, 3);
-fprintf('PSNR for sigma = 5, window = 3')
-psnr_gauss3 = myPSNR(gaussian_img, gauss3);
+gauss8 = denoise(gaussian_img, 'gaussian', 0.8, 3);
+myPSNR(original_img, gauss8)
 
-gauss4 = denoise(gaussian_img, 'gaussian', 7, 3);
-fprintf('PSNR for sigma = 7, window = 3')
-psnr_gauss4 = myPSNR(gaussian_img, gauss4);
+gauss10 = denoise(gaussian_img, 'gaussian', 1, 3);
+myPSNR(original_img, gauss10)
 
-gauss5 = denoise(gaussian_img, 'gaussian', 12, 3);
-fprintf('PSNR for sigma = 12, window = 3')
-psnr_gauss4 = myPSNR(gaussian_img, gauss5);
+gauss2 = denoise(gaussian_img, 'gaussian', 1.2, 3);
+myPSNR(original_img, gauss2)
+
+gauss4 = denoise(gaussian_img, 'gaussian', 1.4, 3);
+myPSNR(original_img, gauss4)
+
+gauss6 = denoise(gaussian_img, 'gaussian', 1.6, 3);
+myPSNR(original_img, gauss6)
+
+gauss8 = denoise(gaussian_img, 'gaussian', 1.8, 3);
+myPSNR(original_img, gauss8)
+
+gauss10 = denoise(gaussian_img, 'gaussian', 2, 3);
+myPSNR(original_img, gauss10)
+
+% % gaussian filtering with varying values of sigma, window = 5
+% 
+gauss2 = denoise(gaussian_img, 'gaussian', 0.2, 5);
+myPSNR(original_img, gauss2)
+
+gauss4 = denoise(gaussian_img, 'gaussian', 0.4, 5);
+myPSNR(original_img, gauss4)
+
+gauss6 = denoise(gaussian_img, 'gaussian', 0.6, 5);
+myPSNR(original_img, gauss6)
+
+gauss8 = denoise(gaussian_img, 'gaussian', 0.8, 5);
+myPSNR(original_img, gauss8)
+
+gauss10 = denoise(gaussian_img, 'gaussian', 1, 5);
+myPSNR(original_img, gauss10)
+
+gauss2 = denoise(gaussian_img, 'gaussian', 1.2, 5);
+myPSNR(original_img, gauss2)
+
+gauss4 = denoise(gaussian_img, 'gaussian', 1.4, 5);
+myPSNR(original_img, gauss4)
+
+gauss6 = denoise(gaussian_img, 'gaussian', 1.6, 5);
+myPSNR(original_img, gauss6)
+
+gauss8 = denoise(gaussian_img, 'gaussian', 1.8, 5);
+myPSNR(original_img, gauss8)
+
+gauss10 = denoise(gaussian_img, 'gaussian', 2, 5);
+myPSNR(original_img, gauss10)
+
+% gaussian filtering with varying values of sigma, window = 7
+
+gauss2 = denoise(gaussian_img, 'gaussian', 0.2, 7);
+myPSNR(original_img, gauss2)
+
+gauss4 = denoise(gaussian_img, 'gaussian', 0.4, 7);
+myPSNR(original_img, gauss4)
+
+gauss6 = denoise(gaussian_img, 'gaussian', 0.6, 7);
+myPSNR(original_img, gauss6)
+
+gauss8 = denoise(gaussian_img, 'gaussian', 0.8, 7);
+myPSNR(original_img, gauss8)
+
+gauss10 = denoise(gaussian_img, 'gaussian', 1, 7);
+myPSNR(original_img, gauss10)
+
+gauss2 = denoise(gaussian_img, 'gaussian', 1.2, 7);
+myPSNR(original_img, gauss2)
+
+gauss4 = denoise(gaussian_img, 'gaussian', 1.4, 7);
+myPSNR(original_img, gauss4)
+
+gauss6 = denoise(gaussian_img, 'gaussian', 1.6, 7);
+myPSNR(original_img, gauss6)
+
+gauss8 = denoise(gaussian_img, 'gaussian', 1.8, 7);
+myPSNR(original_img, gauss8)
+
+gauss10 = denoise(gaussian_img, 'gaussian', 2, 7);
+myPSNR(original_img, gauss10)
+
+
+
+
+
+
 
 
