ce4930732df6e088ec12435b3af46e78e3791127
asnijders <44669359+asnijders@users.noreply.github.com>
2019-09-25 21:52:49 +0200

    Update q7_3_gaussian_filtering.m

--- assign_2/Part-1: Image Enhancement/q7_3_gaussian_filtering.m
+++ assign_2/Part-1: Image Enhancement/q7_3_gaussian_filtering.m
@@ -2,52 +2,17 @@
 % Choose an appropriate window size and standard deviation and justify your choice. 
 % Show the denoised images in your report.
 
+original_img = imread("image1.jpg");
 gaussian_img = imread("image1_gaussian.jpg");
 
-%% gaussian filtering with varying values of sigma, window = 3
-subplot(3,3,1)
-gauss1 = denoise(gaussian_img, 'gaussian', 1, 3);
-imshow(gauss1)
-title("gaussian filtering where sigma = 1, window size = 3")
 
-subplot(3,3,2)
-gauss2 = denoise(gaussian_img, 'gaussian', 2, 3);
-imshow(gauss2)
-title("gaussian filtering where sigma = 2, window size = 3")
+%% gaussian filtering with sigma = 0.6, 0.8, 1.0, 1.2 / window 3, 3, 3, 3, respectively
+gauss1 = denoise(gaussian_img, 'gaussian', 0.4, 3);
 
-subplot(3,3,3)
-gauss3 = denoise(gaussian_img, 'gaussian', 3, 3);
-imshow(gauss3)
-title("gaussian filtering where sigma = 3, window size = 3")
+gauss2= denoise(gaussian_img, 'gaussian', 0.8, 5);
 
-%% gaussian filtering with varying values of sigma, window = 5
-subplot(3,3,4)
-gauss4 = denoise(gaussian_img, 'gaussian', 1, 5);
-imshow(gauss4)
-title("gaussian filtering where sigma = 1, window size = 5")
+gauss3= denoise(gaussian_img, 'gaussian', 2, 5);
 
-subplot(3,3,5)
-gauss5 = denoise(gaussian_img, 'gaussian', 2, 5);
-imshow(gauss5)
-title("gaussian filtering where sigma = 2, window size = 5")
+gauss4 = denoise(gaussian_img, 'gaussian', 4, 7);
 
-subplot(3,3,6)
-gauss6 = denoise(gaussian_img, 'gaussian', 3, 5);
-imshow(gauss6)
-title("gaussian filtering where sigma = 3, window size = 5")
-
-%% gaussian filtering with varying values of sigma, window = 7
-subplot(3,3,7)
-gauss7 = denoise(gaussian_img, 'gaussian', 1, 7);
-imshow(gauss7)
-title("gaussian filtering where sigma = 1, window size = 7")
-
-subplot(3,3,8)
-gauss8 = denoise(gaussian_img, 'gaussian', 2, 7);
-imshow(gauss8)
-title("gaussian filtering where sigma = 2, window size = 7")
-
-subplot(3,3,9)
-gauss9 = denoise(gaussian_img, 'gaussian', 3, 7);
-imshow(gauss9)
-title("gaussian filtering where sigma = 3, window size = 7")
+montage({original_img, gaussian_img, gauss1, gauss2, gauss3, gauss4});
