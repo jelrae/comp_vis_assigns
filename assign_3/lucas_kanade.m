e76d64b1b544592851202a00b14ed9ac71e2b1fa
Jordan <jordanalexearle@gmail.com>
2019-09-27 16:59:47 +0200

    Added a few more things, need to get dx and dy

--- assign_3/lucas_kanade.m
+++ assign_3/lucas_kanade.m
@@ -11,7 +11,20 @@ function flow =  opticFlow(im1, im2, sects = 15)
     im2 = im2(offset_h:h_adjust, offset_w:w_adjust);
     
     %Make the subsections
-    im1 = reshape(im1,sects,sects,[]);
-    im2 = reshape(im2,sects,sects,[]);
+    window1 = reshape(im1,sects,sects,[]);
+    window2 = reshape(im2,sects,sects,[]);
+    %My friend told me about this, she jsut gave it to me.  Not sure if we
+    %should use
+    %window1 = mat2cell(image1, repelem(sects, h_adjust/sects), repelem(sects, w_adjust/sects));
+    %window2 = mat2cell(image1, repelem(sects, h_adjust/sects), repelem(sects, w_adjust/sects));
+    
+    %Make into vectors
+    array1 = reshape(window1,[],1,size(window1,3));
+    array2 = reshape(window2,[],1,size(window2,3));
+    
+    %Make Ix and Iy
+    
+    %Make It
+    b = -1.*(array2.-array1);
     
 end
