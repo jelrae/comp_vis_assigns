22d5461568f20a04cac1364a6e64a761666521e5
Jordan <jordanalexearle@gmail.com>
2019-09-29 16:56:56 +0200

    Changed a few things

--- assign_3/demo_lucas_kanade.m
+++ assign_3/demo_lucas_kanade.m
@@ -18,7 +18,8 @@ function demo_lucas_kanade()
 
     [x, y] = meshgrid(8:15:w_ad, 8:15:h_ad);
 
-    imagesc(unique(x), unique(y), im1);
+%     imagesc(unique(x), unique(y), im1);
+    imshow(im1(1:h_ad,1:w_ad));
     hold on;
     quiver(reshape(x, [], 1), reshape(y, [], 1), flow(:, 1), flow(:, 2), 'color', [1, 1, 1]);
 
