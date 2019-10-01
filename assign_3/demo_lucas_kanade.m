b300d11f17948ede9c92aed0db042dacc768f19f
Jordan <jordanalexearle@gmail.com>
2019-10-01 16:13:44 +0200

    Attempt at tracking

--- assign_3/demo_lucas_kanade.m
+++ assign_3/demo_lucas_kanade.m
@@ -1,6 +1,10 @@
 function demo_lucas_kanade()
+%     % for Sphere
     im1 = im2double(rgb2gray(imread('sphere1.ppm')));
     im2 = im2double(rgb2gray(imread('sphere2.ppm')));
+    %For Synth
+%     im1 = im2double(imread('synth1.pgm'));
+%     im2 = im2double(imread('synth2.pgm'));
     
     sects = 15;
     flow = lucas_kanade(im1,im2,sects);
@@ -18,11 +22,21 @@ function demo_lucas_kanade()
 
     [x, y] = meshgrid(8:15:w_ad, 8:15:h_ad);
 
-%     imagesc(unique(x), unique(y), im1);
-    imshow(im1(1:h_ad,1:w_ad));
+    % Get the plottable Image
+    % for Sphere
+    pltim = imread('sphere1.ppm');
+    % for synth
+%     pltim = imread('synth1.pgm');
+    imshow(pltim);
     hold on;
     quiver(reshape(x, [], 1), reshape(y, [], 1), flow(:, 1), flow(:, 2), 'color', [1, 1, 1]);
-    title('Lucas Kanade Optical Flow of a Sphere');
+    
+    %General Title
+    title('Lucas Kanade Optical Flow');
+    
+    %Specific Titles
+    %title('Lucas Kanade Optical Flow of Synth');
+    %title('Lucas Kanade Optical Flow of a Sphere');
     
 
 end
---
+++ assign_3/tracking_lucas_kanade.m
@@ -0,0 +1,42 @@
+function demo_lucas_kanade()
+%     % for Sphere
+    im1 = im2double(rgb2gray(imread('sphere1.ppm')));
+    im2 = im2double(rgb2gray(imread('sphere2.ppm')));
+    %For Synth
+%     im1 = im2double(imread('synth1.pgm'));
+%     im2 = im2double(imread('synth2.pgm'));
+    
+    sects = 15;
+    flow = lucas_kanade(im1,im2,sects);
+    flow = flow';
+
+    [h, w] = size(im1);
+
+    num_instances = floor(h / sects)^2;
+    h_ad = (floor(h/sects)*sects);
+    w_ad = (floor(w/sects)*sects);
+    
+%     quiver((1:1:num_instances)',(1:1:num_instances)', flow(:,1), flow(:,2))
+    
+    %Friends code
+
+    [x, y] = meshgrid(8:15:w_ad, 8:15:h_ad);
+
+    % Get the plottable Image
+    % for Sphere
+    pltim = imread('sphere1.ppm');
+    % for synth
+%     pltim = imread('synth1.pgm');
+    imshow(pltim);
+    hold on;
+    quiver(reshape(x, [], 1), reshape(y, [], 1), flow(:, 1), flow(:, 2), 'color', [1, 1, 1]);
+    
+    %General Title
+    title('Lucas Kanade Optical Flow');
+    
+    %Specific Titles
+    %title('Lucas Kanade Optical Flow of Synth');
+    %title('Lucas Kanade Optical Flow of a Sphere');
+    
+
+end
