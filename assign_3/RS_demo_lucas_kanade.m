7e90dece8e8d76d562e2c3c1342f7f0ec33aeb4d
Benjamin Wang <wlongxiang1119@gmail.com>
2019-10-01 22:48:34 +0200

    remove redudant files

--- assign_3/RS_demo_lucas_kanade.m
+++
@@ -1,8 +0,0 @@
-function demo_lucas_kanade()
-    %i1 = im2double(rgb2gray(imread('sphere1.ppm')));
-    %i2 = im2double(rgb2gray(imread('sphere2.ppm')));
-    i1 = imread('sphere1.ppm');
-    i2 = imread('sphere2.ppm');
-    sects = 15;
-    RS_lucas_kanade(i1,i2,sects);
-end
